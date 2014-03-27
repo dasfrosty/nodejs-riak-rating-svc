ef9d9f792a88e6a4187b05477a7a449a55bd0a9a
Niels Godfredsen <ngodfredsen@gmail.com>
2014-03-27 00:10:34 -0700

    simplify deem to only provide direct API implementation without REST mapping/routing, which will be implemented through a separate module

---
+++ test/deem.js
@@ -0,0 +1,56 @@
+/*jslint node: true, sloppy: true, indent: 2 */
+
+var should = require('should');
+
+var deemjs = require(__dirname + '/../lib/deem.js');
+
+var riakHostname = 'localhost';
+var riakPort = 8098;
+var riakBaseUrl = 'http://' + riakHostname + ':' + riakPort;
+var riakBucket = 'test_bucket';
+
+
+describe('Deem API', function () {
+
+  var deem = deemjs.createDeem(riakHostname, riakPort, riakBucket);
+
+  it('should load a user that gets put', function (done) {
+    var expected = {userId: 8654, email: 'redbaron@snoopy.com', password: 'sopwithcamel'};
+    deem.putUser(expected.userId, expected.email, expected.password, function (actual) {
+      actual.userId.should.equal(expected.userId);
+      actual.email.should.equal(expected.email);
+      actual.password.should.equal(expected.password);
+      deem.getUser(expected.userId, function (actual) {
+        actual.userId.should.equal(expected.userId);
+        actual.email.should.equal(expected.email);
+        actual.password.should.equal(expected.password);
+        done();
+      });
+    });
+  });
+
+  it('should update a user that gets put', function (done) {
+    var expected = {userId: 8654, email: 'snoopy@redbaron.com', password: '3plane'};
+    deem.putUser(expected.userId, expected.email, expected.password, function (actual) {
+      actual.userId.should.equal(expected.userId);
+      actual.email.should.equal(expected.email);
+      actual.password.should.equal(expected.password);
+      deem.getUser(expected.userId, function (actual) {
+        actual.userId.should.equal(expected.userId);
+        actual.email.should.equal(expected.email);
+        actual.password.should.equal(expected.password);
+        done();
+      });
+    });
+  });
+
+  it('should return null for a user that does not exist', function (done) {
+    deem.getUser(95846474836363, function (actual) {
+      should.not.exist(actual);
+      done();
+    });
+  });
+
+});
+
+
