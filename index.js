fd6738d7dc4f61dc938868df9556c43ec12d766d
Niels Godfredsen <ngodfredsen@gmail.com>
2014-05-14 19:51:28 -0700

    implement putUser REST call

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 /*jslint node: true, sloppy: true, indent: 2 */
 
-var deem = require(__dirname + '/deem.js').createDeem('localhost', 8098).listen(8080);
+var restjs = require(__dirname + '/lib/rest.js');
+
+var riakHostname = 'localhost';
+var riakPort = 8098;
+var riakBucket = 'deem';
+var restPort = 8081;
+
+restjs.createRest(riakHostname, riakPort, riakBucket).listen(restPort);
